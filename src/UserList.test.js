d9b7c84f6b8ae3efaed0cbed56a9edb7927515a7
Simone Taeggi <startae14@gmail.com>
2023-04-06 14:29:23 +0200

    get all the rows of a table

---
+++ src/UserList.test.js
@@ -0,0 +1,27 @@
+import { getAllByRole, render, screen } from '@testing-library/react';
+
+import UserList from './UserList';
+
+test('render one row per user', () => {
+  // Render the component
+  const users = [
+    {
+      name: 'jane',
+      email: 'jane@jane.com'
+    },
+    {
+      name: 'sam',
+      email: 'sam@sam.com'
+    }
+  ];
+  render(<UserList users={users} />)
+  // find all the rows in the table
+  const rows = screen.getAllByRole('row');
+
+  // Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
+});
+
+test('render email and name of each user', () => {
+
+});
