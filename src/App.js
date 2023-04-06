de6ed3f56c7f317f895ae0814b5d795424a357e6
Simone Taeggi <startae14@gmail.com>
2023-04-06 23:46:42 +0200

    testing the App component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div>
       <UserForm onUserAdd={onUserAdd} />
+      <hr />
       <UserList users={users} />
     </div>
   )
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('can receive a new user and show it on a list', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  // find the name input and email
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i
+  });
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i
+  });
+  const button = screen.getByRole('button');
+
+  // simulate click and add value
+  user.click(nameInput);
+  user.keyboard('jane');
+  user.click(emailInput);
+  user.keyboard('jane@jane.com');
+
+  await user.click(button);
+  
+  const name = screen.getByRole('cell', { name: 'jane' });
+  const email = screen.getByRole('cell', { name: 'jane@jane.com' });
+  
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+})
