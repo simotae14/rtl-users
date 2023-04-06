189c1cee01b2b660539f3544bb6feffaa085cd5b
Simone Taeggi <startae14@gmail.com>
2023-04-06 17:57:35 +0200

    fix test duplication

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,28 +2,7 @@ import { render, screen, within } from '@testing-library/react';
 
 import UserList from './UserList';
 
-test('render one row per user', () => {
-  // Render the component
-  const users = [
-    {
-      name: 'jane',
-      email: 'jane@jane.com'
-    },
-    {
-      name: 'sam',
-      email: 'sam@sam.com'
-    }
-  ];
-  render(<UserList users={users} />)
-  // find all the rows in the table
-  const rows = within(screen.getByTestId('users')).getAllByRole('row');
-
-  // Assertion: correct number of rows in the table
-  expect(rows).toHaveLength(2);
-});
-
-test('render email and name of each user', () => {
-  // Render the component
+function renderComponent() {
   const users = [
     {
       name: 'jane',
@@ -36,15 +15,31 @@ test('render email and name of each user', () => {
   ];
   render(<UserList users={users} />);
 
-  for (let user in users) {
-    const name = screen.getByRole('cell', {
-      name: user.name
-    });
-    const email = screen.getByRole('cell', {
-      name: user.email
-    });
+  return {
+    users
+  };
+}
 
+test('render one row per user', () => {
+  // Render the component
+  renderComponent();
+  // find all the rows in the table
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
+  // Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
+});
+
+test('render email and name of each user', () => {
+  // Render the component
+  const {
+    users
+  } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
     expect(name).toBeInTheDocument();
     expect(email).toBeInTheDocument();
-  }
+  };
 });
