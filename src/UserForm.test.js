b70d0a1246bf61f41e553877ff9ea4f935b326a7
Simone Taeggi <startae14@gmail.com>
2023-04-06 12:07:49 +0200

    how to mock a function and test it is called with the correct arguments

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -17,13 +17,9 @@ test('it shows two inputs and a button', () => {
 });
 
 test('it calls onUserAdd when the form is submitted', () => {
-  // NOT THE BEST IMPLEMENTATION
-  const argList = [];
-  const callback = (...args) => {
-    argList.push(args);
-  }
+  const mock = jest.fn();
   // 1. Render the component
-  render(<UserForm onUserAdd={callback} />);
+  render(<UserForm onUserAdd={mock} />);
   // 2. Find the two inputs
   const [nameInput, emailInput] = screen.getAllByRole('textbox');
 
@@ -40,7 +36,9 @@ test('it calls onUserAdd when the form is submitted', () => {
   user.click(button);
 
   // 7. Assertion to make sure 'onUserAdd' gets called with email/name
-  expect(argList).toHaveLength(1);
-  expect(argList[0][0]).toEqual({name: 'jane', email: 'jane@jane.com'});
-
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({
+    name: 'jane',
+    email: 'jane@jane.com'
+  });
 });
