47c7bf25c162dfa85872180c3db7ed4affab371c
Simone Taeggi <startae14@gmail.com>
2023-04-06 16:23:48 +0200

    retrieve the content of a table cell

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 
 import UserList from './UserList';
 
@@ -14,14 +14,37 @@ test('render one row per user', () => {
       email: 'sam@sam.com'
     }
   ];
-  const { container } = render(<UserList users={users} />)
+  render(<UserList users={users} />)
   // find all the rows in the table
-  // eslint-disable-next-line
-  const rows = container.querySelectorAll('tbody tr');
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
   // Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
 
 test('render email and name of each user', () => {
+  // Render the component
+  const users = [
+    {
+      name: 'jane',
+      email: 'jane@jane.com'
+    },
+    {
+      name: 'sam',
+      email: 'sam@sam.com'
+    }
+  ];
+  render(<UserList users={users} />);
 
+  for (let user in users) {
+    const name = screen.getByRole('cell', {
+      name: user.name
+    });
+    const email = screen.getByRole('cell', {
+      name: user.email
+    });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
