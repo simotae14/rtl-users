b968e11585f47d436bb20fde75458b5205c43805
Simone Taeggi <startae14@gmail.com>
2023-04-06 15:31:56 +0200

    get rows content using container.querySelectorAll

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import UserList from './UserList';
 
@@ -14,10 +14,10 @@ test('render one row per user', () => {
       email: 'sam@sam.com'
     }
   ];
-  render(<UserList users={users} />)
+  const { container } = render(<UserList users={users} />)
   // find all the rows in the table
-  const rows = within(screen.getByTestId('users')).getAllByRole('row');
-
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll('tbody tr');
   // Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
